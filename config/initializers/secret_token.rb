583eb7c4142121e626e17a93d91458b15616d624
Christoph Biallas <christoph.biallas@googlemail.com>
2013-07-18 17:08:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sneedin::Application.config.secret_token = '6e50355a07c1bbe561b43f2a1e5b3d4785ee7e9feab7a8336f2b9214d5fb9ff4afc6bd5aeb12d1c8f436632d604d707677089308bbe23fe94702c5bfa9098c86'
